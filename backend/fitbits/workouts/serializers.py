a88320f7f7d448a993bcd89a8e3b2f6b16fa92e8
Animesh Bhargava <55776095+animesh0545@users.noreply.github.com>
2025-04-21 10:59:55 +0530

    Delete backend/fitbits directory

--- backend/fitbits/workouts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Workout
-
-class WorkoutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workout
-        fields = ['id', 'date', 'type', 'duration', 'calories_burnt', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at'] 
